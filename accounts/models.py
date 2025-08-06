252ecba071ebeead1257c576e924f3d78aa71d9c
al-jaber-nishad <mailnishad02@gmail.com>
2025-08-06 19:02:42 +0600

    update accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -22,6 +22,8 @@ class PrimaryGroup(BaseModel):
     name = models.CharField(max_length=100, unique=True)
     is_deletable = models.BooleanField(default=False)
 
+    unique_fields = ['name']
+
     class Meta:
         verbose_name_plural = 'PrimaryGroups'
         ordering = ['-id',]
@@ -43,7 +45,7 @@ class PrimaryGroup(BaseModel):
 
 
 class Group(BaseModel):
-    name = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
 
     head_group = models.ForeignKey('self', on_delete=models.RESTRICT, null=True, blank=True, related_name='children')
     head_primarygroup = models.ForeignKey(PrimaryGroup, on_delete=models.RESTRICT, null=True, blank=True)
@@ -52,8 +54,7 @@ class Group(BaseModel):
     is_primary = models.BooleanField(default=False)
     is_default = models.BooleanField(default=False)
 
-    class Meta:
-        ordering = ['-id',]
+    unique_fields = ['name']
 
     def __str__(self):
         return f"{self.name}"
@@ -79,13 +80,14 @@ class LedgerAccount(BaseModel):
     reference_id = models.CharField(max_length=255, null=True, blank=True)
     details =  models.CharField(max_length=255, null=True, blank=True)
     head_group = models.ForeignKey(Group, on_delete=models.RESTRICT, null=True, blank=True)
-    head_primarygroup = models.ForeignKey(PrimaryGroup, on_delete=models.RESTRICT, null=True, blank=True)
     
     amount = models.DecimalField(default=0, max_digits=50, decimal_places=2,blank=True)
     note = models.TextField(null=True, blank=True)
 
     is_deletable = models.BooleanField(default=True)
     is_default = models.BooleanField(default=False)
+
+    unique_fields = ['name']
 
     class Meta:
         verbose_name_plural = 'LedgerAccounts'
@@ -104,6 +106,8 @@ class LedgerAccount(BaseModel):
 class SubLedgerAccount(BaseModel):
     name = models.CharField(max_length=100, unique=True)
 
+    unique_fields = ['name']
+
     class Meta:
         verbose_name_plural = 'SubLedgerAccounts'
         ordering = ['-id',]
@@ -119,87 +123,53 @@ class SubLedgerAccount(BaseModel):
 
 
 class PaymentVoucher(BaseModel):
-    class Status(models.TextChoices):
-        NULL = 'null', _('null')
-        UPDATE_PENDING = 'update_pending', _('update_pending')
-        DELETE_PENDING = 'delete_pending', _('delete_pending')
-        UPDATE_APPROVED = 'update_approved', _('update_approved')
-        DELETE_APPROVED = 'delete_approved', _('delete_approved')
-        UPDATE_CANCELED = 'update_canceled', _('update_canceled')
-        DELETE_CANCELED = 'delete_canceled', _('delete_canceled')
+    date = models.DateField(null=True, blank=True)
     ledger = models.ForeignKey(LedgerAccount, on_delete=models.RESTRICT, related_name='paymentvoucher')
     sub_ledger = models.ForeignKey(SubLedgerAccount, on_delete=models.RESTRICT, related_name='paymentvoucher', null=True, blank=True)
 
-    is_post_date = models.BooleanField(default=False, null=True, blank=True)
-    cheque_no = models.CharField(max_length=255, null=True, blank=True)
-    payorder_no = models.CharField(max_length=255, null=True, blank=True)
-    cheque_date =  models.DateField(null=True, blank=True)
-    is_cheque = models.CharField(max_length=255, null=True, blank=True)
-
-    inst_no = models.CharField(max_length=255, null=True, blank=True)
+    bank_or_cash = models.BooleanField(default=False, null=True, blank=True)
     bank_name =  models.ForeignKey(Bank, on_delete=models.CASCADE, null=True, blank=True)
-    pdc_note = models.CharField(max_length=255, null=True, blank=True)
-    pdc_type = models.CharField(max_length=255, null=True, blank=True)
-    remarks = models.CharField(max_length=255, null=True, blank=True)
-    favouring_name = models.CharField(max_length=255, null=True, blank=True)
+    cheque_no = models.CharField(max_length=255, null=True, blank=True)
+    cheque_date =  models.DateField(null=True, blank=True)
+    
     debit_amount = models.DecimalField(default=0, max_digits=20, decimal_places=2, blank=True)
     credit_amount = models.DecimalField(default=0, max_digits=20, decimal_places=2, blank=True)
 
     bill_no = models.IntegerField(default=0, null=True, blank=True)
-    bank_or_cash = models.BooleanField(default=False, null=True, blank=True)
-    balance = models.IntegerField(default=0, null=True, blank=True)
-    
     invoice_no = models.CharField(max_length=255, null=True, blank=True)
-    payment_date = models.DateField(null=True, blank=True)
-    file = models.FileField(upload_to='account/PaymentVoucherFile/', null=True, blank=True)
+    
+    file = models.FileField(upload_to='account/PaymentVoucher/', null=True, blank=True)
     details = models.TextField(null=True, blank=True)
 
-    update_status = models.CharField(max_length=15, choices=Status.choices, default=Status.NULL)
 
     class Meta:
         verbose_name_plural = 'PaymentVouchers'
         ordering = ['-id',]
 
     def __str__(self):
-        return f"{self.passenger.passenger_name}"
+        return f"{self.invoice_no}"
 
 
 
 
 class ReceiptVoucher(BaseModel):
-    class Status(models.TextChoices):
-        NULL = 'null', _('null')
-        UPDATE_PENDING = 'update_pending', _('update_pending')
-        DELETE_PENDING = 'delete_pending', _('delete_pending')
-        UPDATE_APPROVED = 'update_approved', _('update_approved')
-        DELETE_APPROVED = 'delete_approved', _('delete_approved')
-        UPDATE_CANCELED = 'update_canceled', _('update_canceled')
-        DELETE_CANCELED = 'delete_canceled', _('delete_canceled')
-    ledger = models.ForeignKey(LedgerAccount, on_delete=models.RESTRICT, related_name='ledger_receiptvoucher')
-    sub_ledger = models.ForeignKey(SubLedgerAccount, on_delete=models.RESTRICT, related_name='receiptvoucher', null=True, blank=True)    
-    cheque_no = models.CharField(max_length=255, null=True, blank=True)
-    payorder_no = models.CharField(max_length=255, null=True, blank=True)
-    cheque_date =  models.DateField(null=True, blank=True)
-    is_cheque = models.CharField(max_length=255, null=True, blank=True)
+    date = models.DateField(null=True, blank=True)
+    ledger = models.ForeignKey(LedgerAccount, on_delete=models.RESTRICT, related_name='receiptvoucher')
+    sub_ledger = models.ForeignKey(SubLedgerAccount, on_delete=models.RESTRICT, related_name='receiptvoucher', null=True, blank=True)
 
-    inst_no = models.CharField(max_length=255, null=True, blank=True)
+    bank_or_cash = models.BooleanField(default=False, null=True, blank=True)
     bank_name =  models.ForeignKey(Bank, on_delete=models.CASCADE, null=True, blank=True)
-    pdc_note = models.CharField(max_length=255, null=True, blank=True)
-    pdc_type = models.CharField(max_length=255, null=True, blank=True)
-    remarks = models.CharField(max_length=255, null=True, blank=True)
-    favouring_name = models.CharField(max_length=255, null=True, blank=True)
+    cheque_no = models.CharField(max_length=255, null=True, blank=True)
+    cheque_date =  models.DateField(null=True, blank=True)
+    
     debit_amount = models.DecimalField(default=0, max_digits=20, decimal_places=2, blank=True)
     credit_amount = models.DecimalField(default=0, max_digits=20, decimal_places=2, blank=True)
-    bill_no = models.IntegerField(default=0, null=True, blank=True)
-    bank_or_cash = models.BooleanField(default=False, null=True, blank=True)
-    balance = models.IntegerField(default=0, null=True, blank=True)
-    
-    invoice_no = models.CharField(max_length=255, null=True, blank=True)
-    receipt_date = models.DateField(null=True, blank=True)
-    file = models.FileField(upload_to='account/ReceiptVoucherFile/', null=True, blank=True)
-    details = models.TextField(null=True, blank=True)
 
-    update_status = models.CharField(max_length=15, choices=Status.choices, default=Status.NULL)
+    bill_no = models.IntegerField(default=0, null=True, blank=True)
+    invoice_no = models.CharField(max_length=255, null=True, blank=True)
+    
+    file = models.FileField(upload_to='account/PaymentVoucher/', null=True, blank=True)
+    details = models.TextField(null=True, blank=True)
 
     class Meta:
         verbose_name_plural = 'ReceiptVouchers'
@@ -211,30 +181,20 @@ class ReceiptVoucher(BaseModel):
 
 
 class Contra(BaseModel):
-    class Status(models.TextChoices):
-        NULL = 'null', _('null')
-        UPDATE_PENDING = 'update_pending', _('update_pending')
-        DELETE_PENDING = 'delete_pending', _('delete_pending')
-        UPDATE_APPROVED = 'update_approved', _('update_approved')
-        DELETE_APPROVED = 'delete_approved', _('delete_approved')
-        UPDATE_CANCELED = 'update_canceled', _('update_canceled')
-        DELETE_CANCELED = 'delete_canceled', _('delete_canceled')
-    
+    date = models.DateTimeField(null=True, blank=True)
     ledger = models.ForeignKey(LedgerAccount, on_delete=models.RESTRICT, related_name='contras')
-    cheque_no = models.CharField(max_length=255, null=True, blank=True)
-    payorder_no = models.CharField(max_length=255, null=True, blank=True)
+    
+    bank_or_cash = models.BooleanField(default=False, null=True, blank=True)
     bank_name =  models.ForeignKey(Bank, on_delete=models.CASCADE, null=True, blank=True)
+    cheque_no = models.CharField(max_length=255, null=True, blank=True)
     cheque_date =  models.DateField(null=True, blank=True)
     
-    bill_no = models.IntegerField(default=0, null=True, blank=True)
-    bank_or_cash = models.BooleanField(default=False, null=True, blank=True)
-    balance = models.IntegerField(default=0, null=True, blank=True)
-
-    invoice_no = models.CharField(max_length=255, null=True, blank=True)
-
     debit_amount = models.DecimalField(default=0, max_digits=20, decimal_places=2, blank=True)
     credit_amount = models.DecimalField(default=0, max_digits=20, decimal_places=2, blank=True)
-    contra_date = models.DateTimeField(null=True, blank=True)
+
+    bill_no = models.IntegerField(default=0, null=True, blank=True)
+    invoice_no = models.CharField(max_length=255, null=True, blank=True)
+
     details = models.TextField(null=True, blank=True)
 
     class Meta:
@@ -246,13 +206,16 @@ class Contra(BaseModel):
 
 
 class AccountLog(BaseModel):
+    date = models.DateTimeField(null=True, blank=True)
     ledger = models.ForeignKey(LedgerAccount, on_delete=models.RESTRICT, related_name='account_logs', null=True, blank=True)
+    
     sub_ledger = models.ForeignKey(SubLedgerAccount, on_delete=models.RESTRICT, related_name='account_logs', null=True, blank=True)
     reference_no = models.CharField(max_length=255, null=True, blank=True)
     log_type = models.CharField(max_length=20, null=True, blank=True)
+    
     debit_amount = models.DecimalField(default=0, max_digits=20, decimal_places=2, null=True, blank=True)
     credit_amount = models.DecimalField(default=0, max_digits=20, decimal_places=2, null=True, blank=True)
-    log_date = models.DateTimeField(null=True, blank=True)
+    
     details = models.TextField(null=True, blank=True)
     
     class Meta:
